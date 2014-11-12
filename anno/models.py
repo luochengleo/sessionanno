935fd695a94115de96761085a6733d5c54bda228
luochengleo <luochengleo@gmail.com>
2014-11-12 17:20:23 +0800

    Add

--- anno/models.py
+++ anno/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class SearchResult(models.Model):
+    query = models.CharField(max_length=100)
+    rank = models.IntegerField()
+    content = models.CharField(max_length=2000)
+
+
