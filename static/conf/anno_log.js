6a5a985eebce97d8f14f6de7f5981ff034009026
defaultstr <maojiaxin@gmail.com>
2014-12-02 20:46:00 +0800

    Add checkbox for annolist and tasks.

--- static/conf/anno_log.js
+++ static/conf/anno_log.js
@@ -27,6 +27,19 @@ $(function () {
     }
 });
 
+$(function () {
+    $(".annotation_link").click(function () {
+        var node = $(this).get(0);
+        var parent_node = node.parentNode;
+        while (parent_node != null) {
+            if (parent_node.className == "annotation_cell") {
+                $(parent_node).find("img").css("visibility", "visible");
+            }
+            parent_node = parent_node.parentNode;
+        }
+    });
+});
+
 function questionnaire_button_on_click() {
     var text = $("#answer").val();
     var message = "";
