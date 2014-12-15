0ded313f47c4ef3032af7e2b9d2cf9b45b551acb
defaultstr <maojiaxin@gmail.com>
2014-12-15 16:09:19 +0800

    Add tasks.xls

--- Utils/AddTask.py
+++ Utils/AddTask.py
@@ -8,5 +8,5 @@ def importTasks(filename):
         for line in fin:
             task_id, init_query, content,question = line.split(',')
             task_id = int(task_id)
-            t = Task(content=content, task_id=task_id, init_query=init_query,question=question)
+            t = Task(content=content, task_id=task_id, init_query=init_query, question=question)
             t.save()
